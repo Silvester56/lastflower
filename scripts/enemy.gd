85ab0e6bea31d9a125d745252244c95d9648ddfe
Silvester56 <vargfoad@gmail.com>
2025-03-18 17:44:40 +0100

    Add melee weapon for player and health to enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,11 +2,18 @@ extends CharacterBody2D
 
 var goal: Vector2
 var speed = 50.0
+var isEnemy = true
+var health: int = 2
 
 func _physics_process(delta: float) -> void:
 	var direction = (goal - position).normalized()
 	velocity = direction * speed
 	move_and_collide(velocity * delta)
+
+func getHit(damage: int):
+	health = health - damage
+	if health <= 0:
+		queue_free()
 
 func _on_timer_timeout() -> void:
 	$Sprite2D.frame = ($Sprite2D.frame + 1) % 2
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 var speed = 100.0
+var meleePower = 1
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("melee"):
+		$Range/Polygon2D.visible = true
+		$Range/Timer.start()
+		for body in $Range.get_overlapping_bodies():
+			if "isEnemy" in body and body.isEnemy:
+				body.getHit(meleePower)
 	var directionX := Input.get_axis("left", "right")
 	if directionX:
 		velocity.x = directionX * speed
@@ -15,3 +22,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	$Range/Polygon2D.visible = false
