1887c19a820c03e154158b018560f6bf82773f31
Silvester56 <vargfoad@gmail.com>
2025-03-18 22:46:04 +0100

    Add main menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -21,4 +21,5 @@ func _on_retry_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_back_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
