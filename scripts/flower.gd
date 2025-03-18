09ebc5f32a64b5d8e3c04a40d672ea38f34aa2fa
Silvester56 <vargfoad@gmail.com>
2025-03-18 21:54:20 +0100

    Add health for flower

---
+++ scripts/flower.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var health: int = 100
+
+func _physics_process(delta: float) -> void:
+	for body in get_overlapping_bodies():
+		if "isEnemy" in body and body.isEnemy:
+			changeHealth(-1)
+
+func changeHealth(ammount):
+	health = health + ammount
+	$Health.value = health
