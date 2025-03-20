63b1ad1970d96c557bbb49982006edc0cebf4d1b
Silvester56 <vargfoad@gmail.com>
2025-03-20 12:45:54 +0100

    Add how to play screen

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,10 +4,13 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_how_to_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
